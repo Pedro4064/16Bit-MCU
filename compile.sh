18751894d53ece0af9bdb7c0f7941421de98eafd
Pedro4064 <pedrohlcruz@gmail.com>
2023-07-01 09:58:28 -0300

    [IMPLEMENTED] Echo to aid build debug

--- compile.sh
+++ compile.sh
@@ -1,10 +1,10 @@
-ghdl -a --std=08 hdl/decodificador_3_8.vhd &&
-ghdl -a --std=08 hdl/FSM.vhd &&
-ghdl -a --std=08 hdl/n_register.vhd &&
-ghdl -a --std=08 hdl/mux.vhd &&
-ghdl -a --std=08 hdl/addsub.vhd && 
-ghdl -a --std=08 hdl/mcu.vhd && 
-ghdl -a --std=08 test/pkg_testing.vhd && 
-ghdl -a --std=08 test/tb_mcu.vhd && 
-ghdl -e --std=08 tb_mcu &&
-ghdl -r --std=08 tb_mcu --wave=wave_mcu.ghw
+ghdl -a --std=08 hdl/decodificador_3_8.vhd && echo "[x] decodificador_3_8.vhd" &&
+ghdl -a --std=08 hdl/FSM.vhd && echo "[x] FSM.vhd" &&
+ghdl -a --std=08 hdl/n_register.vhd && echo "[x] n_register.vhd" &&
+ghdl -a --std=08 hdl/mux.vhd && echo "[x] mux.vhd" &&
+ghdl -a --std=08 hdl/addsub.vhd &&  echo "[x] addsub.vhd" &&
+ghdl -a --std=08 hdl/mcu.vhd &&  echo "[x] mcu.vhd" &&
+ghdl -a --std=08 test/pkg_testing.vhd &&  echo "[x] pkg_testing.vhd" &&
+ghdl -a --std=08 test/tb_mcu.vhd &&  echo "[x] tb_mcu.vhd" &&
+ghdl -e --std=08 tb_mcu && echo "[x] e -- tb_mcu"
+ghdl -r -v --std=08 tb_mcu --wave=wave_mcu.ghw

ec8553af96c6b2b8c5dd5636028ee861186bae34
Pedro4064 <pedrohlcruz@gmail.com>
2023-06-30 23:41:59 -0300

    [FIXED] mux syntax error

--- hdl/mux.vhd
+++ hdl/mux.vhd
@@ -24,22 +24,16 @@ END mux;
 
 ARCHITECTURE arch OF mux IS
 BEGIN
-
-  MUX_SELECT : PROCESS
-  BEGIN
-
-    WITH i_bus_select SELECT o_data_out <=
-      "0000000000000001"WHEN "0000000001",
-      "0000000000000001" WHEN OTHERS;
-    --     i_data_bus_01 WHEN "0000000010",
-    --     i_data_bus_02 WHEN "0000000100",
-    --     i_data_bus_03 WHEN "0000001000",
-    --     i_data_bus_04 WHEN "0000010000",
-    --     i_data_bus_05 WHEN "0000100000",
-    --     i_data_bus_06 WHEN "0001000000",
-    --     i_data_bus_07 WHEN "0010000000",
-    --     i_data_bus_08 WHEN "0100000000",
-    --     i_data_bus_09 WHEN "1000000000"
-  END PROCESS; -- MUX_SELECT
+  WITH i_bus_select SELECT o_data_out <=
+    i_data_bus_00 WHEN "0000000001",
+    i_data_bus_01 WHEN "0000000010",
+    i_data_bus_02 WHEN "0000000100",
+    i_data_bus_03 WHEN "0000001000",
+    i_data_bus_04 WHEN "0000010000",
+    i_data_bus_05 WHEN "0000100000",
+    i_data_bus_06 WHEN "0001000000",
+    i_data_bus_07 WHEN "0010000000",
+    i_data_bus_08 WHEN "0100000000",
+    i_data_bus_09 WHEN OTHERS;
 
 END arch; -- arch
